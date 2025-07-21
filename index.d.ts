6e863322a193284d1a3bdb5a8ad9c0bfc32dbcb3
Ramy Hadid <ramyhadid.js@gmail.com>
2025-07-21 12:39:37 +0100

    IC

--- index.d.ts
+++ index.d.ts
@@ -1,30 +1,33 @@
-declare module 'vanilla-tilt-svelte' {
-    export interface SSVTProps {
-        reverse: boolean;
-        max: number;
-        startX: number;
-        startY: number;
-        perspective: number;
-        scale: number;
-        speed: number;
-        transition: boolean;
-        axis: string | null;
-        reset: boolean;
-        resetToStart: boolean;
-        easing: string;
-        glare: boolean;
-        maxGlare: number;
-        glarePrerender: boolean;
-        mouseEventElement: string | null;
-        gyroscope: boolean;
-        gyroscopeMinAngleX: number;
-        gyroscopeMaxAngleX: number;
-        gyroscopeMinAngleY: number;
-        gyroscopeMaxAngleY: number;
-    }
+declare module "vanilla-tilt-svelte" {
+  export interface SSVTProps {
+    reverse: boolean;
+    max: number;
+    startX: number;
+    startY: number;
+    perspective: number;
+    scale: number;
+    speed: number;
+    transition: boolean;
+    axis: string | null;
+    reset: boolean;
+    resetToStart: boolean;
+    easing: string;
+    glare: boolean;
+    maxGlare: number;
+    glarePrerender: boolean;
+    mouseEventElement: string | null;
+    gyroscope: boolean;
+    gyroscopeMinAngleX: number;
+    gyroscopeMaxAngleX: number;
+    gyroscopeMinAngleY: number;
+    gyroscopeMaxAngleY: number;
+  }
 
-    export default function svelteTiltRHPO(node: Node, settingsObject?: SSVTProps): {
-    	update(newSettings: SSVTProps): void;
-    	destroy(): void;
-    };
+  export default function svelteTilt(
+    node: Node,
+    settingsObject?: SSVTProps
+  ): {
+    update(newSettings: SSVTProps): void;
+    destroy(): void;
+  };
 }
